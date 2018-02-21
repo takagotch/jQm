b0c5a09bd85dbdfef37537e1e2e6970955d78fc1
takagotch <dyaccb@gmail.com>
2018-02-21 19:35:53 +0900

    .buttonMarkup({ icon: 'home', iconpos: 'notext', });

--- btn/btn.js
+++ btn/btn.js
@@ -4,4 +4,12 @@ $('#button-disable').button('disable');
 $('#button-enable').removeClass('ui-disabled');
 $('#button-disable').addClass('ui-disabled');
 
+$('div').live('pagecreate', function(e){
+  $('a.home').buttonMarkup({
+    icon: 'home',
+    iconpos: 'notext',
+    inline: true,
+  });
+});
+
 
