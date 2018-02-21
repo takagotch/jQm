12a59db4ddcb8b09b6f54f323fcb649159ee7d0e
takagotch <dyaccb@gmail.com>
2018-02-21 19:40:50 +0900

    .appendTo(form).trigger(create); s.selectmenu();

---
+++ form/form.js
@@ -0,0 +1,12 @@
+$("#slider").val(38).slider("refresh");
+
+$("#check").attr("checked", true).checkboxradio("refresh");
+
+var s = $("#select");
+s[0].selectedIndex = 2;
+s.selectmenu("refresh");
+
+var markup = '<div ...>';
+$(markup).appendTo("form").trigger("create");
+
+
